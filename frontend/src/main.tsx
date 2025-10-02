662fecb72181ac435c810c70006f7b0a967f6930
Agustin Loos <loosagustin@gmail.com>
2025-10-02 17:10:30 -0300

    feat: agregar enrutamiento a NoticiasPage en main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import App from './App.tsx';
+import NoticiasPage from './pages/NoticiasPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/noticias" replace />} />
+        <Route path="/noticias" element={<NoticiasPage />} />
+        <Route path="/demo" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
