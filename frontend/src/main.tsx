a3660a22a8bcda02d77aa710d39c28196e1be6fd
Agustin Loos <loosagustin@gmail.com>
2025-10-03 11:58:02 -0300

    refactor: ajustar importaciones y rutas en main.tsx para mejorar la estructura del proyecto

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import App from './App.tsx';
-import NoticiasPage from './pages/NoticiasPage';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import NoticiasPage from "./pages/NoticiasPage";
+import DetallePage from "./pages/DetallePage";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/noticias" replace />} />
         <Route path="/noticias" element={<NoticiasPage />} />
-        <Route path="/demo" element={<App />} />
+        <Route path="/detalle/:id" element={<DetallePage />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
