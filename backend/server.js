85f238764628573571a832ecb5fe6b1f9272542f
agustinso0 <loosagustin@gmail.com>
2025-11-15 15:02:29 -0300

    refactor: mejorar el formato y estilo en server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const authMiddleware = require('./middleware/auth');
-const getDatabase = require('./db');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authMiddleware = require("./middleware/auth");
+const getDatabase = require("./db");
 
 // require controllers
-const NewController = require('./controllers/NewsController');
+const NewController = require("./controllers/NewsController");
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = getDatabase();
 
 // TESTEO: verificar conexion a la base de datos
-app.get('/test', (req, res) => {
-    db.all('SELECT * FROM noticias', (err, row) => {
-        if (err) {
-            res.status(500).json({ error: 'Error al conectar a la base de datos' });
-        } else {
-            res.json({ message: 'Conexión a la base de datos exitosa', row });
-        }
-    });
+app.get("/test", (req, res) => {
+  db.all("SELECT * FROM noticias", (err, row) => {
+    if (err) {
+      res.status(500).json({ error: "Error al conectar a la base de datos" });
+    } else {
+      res.json({ message: "Conexión a la base de datos exitosa", row });
+    }
+  });
 });
 
 // routes
-app.post('/noticias', NewController.create);
-app.get('/noticias', NewController.getAll);
-app.get('/noticias/:id', NewController.getById);
-app.put('/noticias/:id', NewController.update);
-app.delete('/noticias/:id', NewController.delete);
-app.post('/login', authMiddleware.authenticate, (req, res) => {
-    res.json({ message: 'Login exitoso' });
+app.post("/noticias", NewController.create);
+app.get("/noticias", NewController.getAll);
+app.get("/noticias/:id", NewController.getById);
+app.put("/noticias/:id", NewController.update);
+app.delete("/noticias/:id", NewController.delete);
+app.post("/login", authMiddleware.authenticate, (req, res) => {
+  res.json({ message: "Login exitoso" });
 });
 
 // Exportamos `app` para tests
@@ -41,5 +41,7 @@ module.exports = app;
 // Solo arrancar servidor si no es test
 if (require.main === module) {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server running on port localhost:${PORT}`));
+  app.listen(PORT, () =>
+    console.log(`Server running on port localhost:${PORT}`)
+  );
 }
