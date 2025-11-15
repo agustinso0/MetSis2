7407e530ae214671fefe670e9fb750147054aaf7
agustinso0 <loosagustin@gmail.com>
2025-11-15 15:02:33 -0300

    refactor: mejorar el formato y estilo en auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,16 +1,20 @@
-const getDatabase = require('../db');
+const getDatabase = require("../db");
 
 const db = getDatabase();
 
 exports.authenticate = (req, res, next) => {
-    const { username, password } = req.body;
-    db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
-        if (err) {
-            res.status(500).json({ error: 'Error al autenticar al usuario' });
-        } else if (row) {
-            next();
-        } else {
-            res.status(401).json({ error: 'Credenciales inválidas' });
-        }
-    });
-}
+  const { username, password } = req.body;
+  db.get(
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    [username, password],
+    (err, row) => {
+      if (err) {
+        res.status(500).json({ error: "Error al autenticar al usuario" });
+      } else if (row) {
+        next();
+      } else {
+        res.status(401).json({ error: "Credenciales inválidas" });
+      }
+    }
+  );
+};
