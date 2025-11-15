bbf91a83914e94358762f6f3b07b4685a236e631
agustinso0 <loosagustin@gmail.com>
2025-11-15 15:02:39 -0300

    refactor: corregir la indentación en el modelo News

--- backend/models/News.js
+++ backend/models/News.js
@@ -1,15 +1,15 @@
 // modelo New
 
 class News {
-    constructor(id, titulo, contenido, autor, imagen, updated_at, publicado) {
-        this.id = id;
-        this.titulo = titulo;
-        this.contenido = contenido;
-        this.autor = autor;
-        this.imagen = imagen;
-        this.updated_at = updated_at;
-        this.publicado = publicado;
-    }
+  constructor(id, titulo, contenido, autor, imagen, updated_at, publicado) {
+    this.id = id;
+    this.titulo = titulo;
+    this.contenido = contenido;
+    this.autor = autor;
+    this.imagen = imagen;
+    this.updated_at = updated_at;
+    this.publicado = publicado;
+  }
 }
 
 module.exports = News;
