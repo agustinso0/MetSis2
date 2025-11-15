5815e93195944b34bdec955075811adb83e5ed99
agustinso0 <loosagustin@gmail.com>
2025-11-15 15:06:43 -0300

    feat: agregar archivo index para exportar componentes UI

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,8 @@
+// Exportar todos los componentes UI
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Input } from "./Input";
+export { default as Textarea } from "./Textarea";
+export { default as LoadingSpinner, LoadingState } from "./LoadingSpinner";
+export { default as ErrorState } from "./ErrorState";
+export { default as Badge } from "./Badge";
