d4c1b582e9e04e0d075652de38bdb02e36bea15d
agustinso0 <loosagustin@gmail.com>
2025-11-15 15:02:48 -0300

    refactor: corregir el formato de comillas y mejorar la legibilidad en NewsRepository.js

--- backend/repositorios/NewsRepository.js
+++ backend/repositorios/NewsRepository.js
@@ -1,4 +1,4 @@
-const getDatabase = require('../db');
+const getDatabase = require("../db");
 
 class NewsRepository {
   constructor() {
@@ -6,28 +6,38 @@ class NewsRepository {
   }
   //CREATE
   create(news, callback) {
-    const sql = 'INSERT INTO noticias (titulo, contenido, autor, imagen) VALUES (?, ?, ?, ?)';
-    this.db.run(sql, [news.titulo, news.contenido, news.autor, news.imagen], function(err) {
-      callback(err, this.lastID);
-    });
+    const sql =
+      "INSERT INTO noticias (titulo, contenido, autor, imagen) VALUES (?, ?, ?, ?)";
+    this.db.run(
+      sql,
+      [news.titulo, news.contenido, news.autor, news.imagen],
+      function (err) {
+        callback(err, this.lastID);
+      }
+    );
   }
   //GETTERS
   getAll(callback) {
-    this.db.all('SELECT * FROM noticias', callback);
+    this.db.all("SELECT * FROM noticias", callback);
   }
 
   getById(id, callback) {
-    this.db.get('SELECT * FROM noticias WHERE id = ?', [id], callback);
+    this.db.get("SELECT * FROM noticias WHERE id = ?", [id], callback);
   }
   //UPDATE
   update(id, news, callback) {
-    const sql = 'UPDATE noticias SET titulo = ?, contenido = ?, autor = ?, imagen = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?';
-    this.db.run(sql, [news.titulo, news.contenido, news.autor, news.imagen, id], callback);
+    const sql =
+      "UPDATE noticias SET titulo = ?, contenido = ?, autor = ?, imagen = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?";
+    this.db.run(
+      sql,
+      [news.titulo, news.contenido, news.autor, news.imagen, id],
+      callback
+    );
   }
   //DELETE
   delete(id, callback) {
-    this.db.run('DELETE FROM noticias WHERE id = ?', [id], callback);
+    this.db.run("DELETE FROM noticias WHERE id = ?", [id], callback);
   }
 }
 
-module.exports = new NewsRepository();  // Instancia única
+module.exports = new NewsRepository(); // Instancia única
