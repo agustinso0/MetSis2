096f5d2334fc51a31e93a74a9c45cbe6c46fdc59
agustinso0 <loosagustin@gmail.com>
2025-11-15 15:02:24 -0300

    refactor: corregir formato y estilo en db.js

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,16 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
 // instancia SINGLETON
-let database
+let database;
 
 function getDatabase() {
-    if (!database) {
-        database = new sqlite3.Database('./database.db');
-        return database;
-    } else {
-        console.log('Base de datos ya existe');
-        return database;
-    }
+  if (!database) {
+    database = new sqlite3.Database("./database.db");
+    return database;
+  } else {
+    console.log("Base de datos ya existe");
+    return database;
+  }
 }
 
 module.exports = getDatabase;
