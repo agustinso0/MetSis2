2f7faca38cdd4a1d6e4266171ce0aa671d9da952
agustinso0 <loosagustin@gmail.com>
2025-11-16 15:34:23 -0300

    refactor: actualizar la configuración de Jest para mejorar la compatibilidad con TypeScript

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -2,16 +2,17 @@ module.exports = {
   testEnvironment: "jsdom",
   roots: ["<rootDir>/src"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
-  globals: {
-    "ts-jest": {
+    "^.+\\.(ts|tsx)$": ["ts-jest", {
       tsconfig: {
         jsx: "react-jsx",
         allowJs: true,
+        target: "es2020",
+        lib: ["es2020", "dom"],
+        module: "esnext",
+        moduleResolution: "node",
       },
-    },
+    }],
   },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
