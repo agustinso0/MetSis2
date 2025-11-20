4c08db7535018c7035cc4f2f10fb1e76d66b4061
agustinso0 <loosagustin@gmail.com>
2025-11-20 14:19:37 -0300

    fix: actualizar la configuración de Jest para usar el módulo ESNext y agregar archivos de configuración

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
   roots: ["<rootDir>/src"],
+  setupFiles: ["<rootDir>/jest.setup.js"],
   transform: {
     "^.+\\.(ts|tsx)$": ["ts-jest", {
       tsconfig: {
@@ -8,9 +9,10 @@ module.exports = {
         allowJs: true,
         target: "es2020",
         lib: ["es2020", "dom"],
-        module: "commonjs",
+        module: "esnext",
         moduleResolution: "node",
         types: ["jest", "@testing-library/jest-dom", "node"],
+        typeRoots: ["./node_modules/@types", "./jest.d.ts"],
       },
     }],
   },
